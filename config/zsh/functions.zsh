b2d0f921f3d78ed1513b6631ff352da75d598066
Wilfried AGO <wilfriedago@pm.me>
2024-08-23 11:19:24 +0100

    feat: automatically switch node versions with nvm in zsh
    - streamline node version management by auto-switching in directories with `.nvmrc`
    - revert to default node version when leaving such directories

--- config/zsh/functions.zsh
+++ config/zsh/functions.zsh
@@ -91,7 +91,28 @@ load-nvmrc() {
   fi
 }
 
-# Call `nvm use` automatically in a directory with a `.nvmrc` file
+# https://github.com/nvm-sh/nvm#zsh
 autoload -U add-zsh-hook
+
+# Call `nvm use` automatically in a directory with a `.nvmrc` file
+load-nvmrc() {
+  local nvmrc_path
+  nvmrc_path="$(nvm_find_nvmrc)"
+
+  if [ -n "$nvmrc_path" ]; then
+    local nvmrc_node_version
+    nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+
+    if [ "$nvmrc_node_version" = "N/A" ]; then
+      nvm install
+    elif [ "$nvmrc_node_version" != "$(nvm version)" ]; then
+      nvm use
+    fi
+  elif [ -n "$(PWD=$OLDPWD nvm_find_nvmrc)" ] && [ "$(nvm version)" != "$(nvm version default)" ]; then
+    echo "Reverting to nvm default version"
+    nvm use default
+  fi
+}
+
 type -a nvm > /dev/null && add-zsh-hook chpwd load-nvmrc
 type -a nvm > /dev/null && load-nvmrc
