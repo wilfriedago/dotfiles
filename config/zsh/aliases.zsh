628eaa30bfe0df95000921d1973f53c5a8e31440
Wilfried AGO <wilfriedago@pm.me>
2025-02-13 17:03:52 +0100

    refactor(zsh): remove corepack aliases for npm and yarn
    - simplifies alias configuration
    - reduces redundancy in package manager commands

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -1,10 +1,6 @@
 # corepack
-alias yarn='corepack yarn'
-alias yarnpkg='corepack yarnpkg'
 alias pnpm='corepack pnpm'
 alias pnpx='corepack pnpx'
-alias npm='corepack npm'
-alias npx='corepack npx'
 
 # pnpm
 alias pn='pnpm'
