d2432d8ace80337b62c818928c214f8c0360f441
Wilfried AGO <wilfriedago@pm.me>
2025-05-11 09:23:53 +0100

    feat(zsh): update completions and remove style config
    - add zstyle configurations for fzf-tab and git completion
    - remove style.zsh as its settings are now in completions.zsh
    - ensure hidden files are included in globbing

--- config/zsh/completions.zsh
+++ config/zsh/completions.zsh
@@ -1,2 +1,32 @@
 # Load Angular CLI autocompletion.
 source <(ng completion script)
+
+# Make sure hidden files are included in globbing
+setopt globdots
+
+# Uncomment one of the following lines to change the auto-update behavior
+# zstyle ':omz:update' mode disabled  # disable automatic updates
+# zstyle ':omz:update' mode auto      # update automatically without asking
+# zstyle ':omz:update' mode reminder  # just remind me to update when it's time
+
+# Uncomment the following line to change how often to auto-update (in days).
+# zstyle ':omz:update' frequency 13
+
+zstyle ':completion:*' fzf-search-display true
+# disable sort when completing `git checkout`
+zstyle ':completion:*:git-checkout:*' sort false
+# set descriptions format to enable group support
+# NOTE: don't use escape sequences here, fzf-tab will ignore them
+zstyle ':completion:*:descriptions' format '[%d]'
+# set list-colors to enable filename colorizing
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+# force zsh not to show completion menu, which allows fzf-tab to capture the unambiguous prefix
+zstyle ':completion:*' menu no
+# hide parents
+zstyle ':completion:*' ignored-patterns '.|..|.DS_Store|**/.|**/..|**/.DS_Store|**/.git'
+# hide `..` and `.` from file menu
+zstyle ':completion:*' ignore-parents 'parent pwd directory'
+# preview directory's content with eza when completing cd
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
+# switch group using `<` and `>`
+zstyle ':fzf-tab:*' switch-group '<' '>'
