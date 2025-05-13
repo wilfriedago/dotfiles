7bbd6ccee020e493c009463508fb78ec934f4dd3
Wilfried AGO <wilfriedago@pm.me>
2025-05-13 07:43:20 +0100

    feat(aliases.zsh): add global directory aliases and update ls commands
    - adds global aliases for navigating directories quickly
    - updates ls aliases to use eza with icons for better visibility

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -1,3 +1,21 @@
+# directories
+alias -g ..='..'
+alias -g ...='../..'
+alias -g ....='../../..'
+alias -g .....='../../../..'
+alias -g ......='../../../../..'
+
+alias -- -='cd -'
+alias 1='cd -1'
+alias 2='cd -2'
+alias 3='cd -3'
+alias 4='cd -4'
+alias 5='cd -5'
+alias 6='cd -6'
+alias 7='cd -7'
+alias 8='cd -8'
+alias 9='cd -9'
+
 # corepack
 alias pnpm='corepack pnpm'
 alias pnpx='corepack pnpx'
@@ -75,11 +93,10 @@ alias mkclean='minikube stop && minikube delete'
 alias mkrestart='minikube stop && minikube start'
 alias mkrg='minikube addons enable registry'
 
-# exa
+# eza
+alias l="eza -al --icons" # ls -l with icons
 alias ls='eza -a --icons --level=1' # ls with icons
-alias ll="eza -al --icons" # ls -l with icons
-alias l="eza -al --icons --git" # ls -l with icons and git status
-alias lt="eza -al --icons --tree --level=3  --git" # ls tree with icons and git status
+alias lt="eza -al --icons --level=3 --tree" # ls tree with icons
 
 # dotdrop
 alias dot='dotdrop --profile=default --cfg=~/.dotfiles/dotdrop.config.yml' # dotdrop with default profile
