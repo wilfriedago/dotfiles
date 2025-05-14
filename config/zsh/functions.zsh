c39290fb1ac8a6eaf199dcfa9616bbb8145c8e3e
Wilfried AGO <wilfriedago@pm.me>
2025-05-14 09:35:10 +0100

    feat(functions): add lazy load ssh agent and clone function
    - introduces a function to lazy load the ssh agent if not already running
    - adds a function to clone a repository from a given url and cd into it

--- config/zsh/functions.zsh
+++ config/zsh/functions.zsh
@@ -77,3 +77,18 @@ clean_path() {
 
   export PATH="${(j/:/)clean_parts}"
 }
+
+# Lazy load SSH agent
+function _load_ssh_agent() {
+  if [ -z "$$SSH_AUTH_SOCK" ]; then
+    eval "$(ssh-agent -s)" > /dev/null
+    ssh-add ~/.ssh/id_github_sign_and_auth 2>/dev/null
+  fi
+}
+
+# clone a repo from url (https|git) and cd into it
+function clone() {
+	local url=$1;
+	local repo=$(echo $url | awk -F/ '{print $NF}' | sed -e 's/.git$//');
+	git clone $url $repo && cd $repo;
+}
