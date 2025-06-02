9e22c0cd9395d446fd66472164e4f007ee9220d3
Wilfried AGO <68906789+wilfriedago@users.noreply.github.com>
2025-06-02 19:04:53 +0100

    feat(aliases): add alias for local Claude Code
    - adds an alias to run local Claude Code
    - improves convenience for accessing the tool

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -86,6 +86,7 @@ alias flushdns="sudo killall -HUP mDNSResponder" # flush dns cache
 alias pubkey="cat ~/.ssh/id_ed25519.pub | pbcopy | echo 'Public Key => Copied to clipboard.'" # copy ssh public key to clipboard
 alias lzd="lazydocker" # run Docker TUI
 alias yz='yazi' # run yazi
+alias claude="~/.claude/local/claude" # run local Claude Code
 
 # Get macOS Software Updates, and update installed Homebrew, npm, and their installed packages
 alias update='sudo softwareupdate -i -a; brew update; brew upgrade; brew cleanup; pnpm add -g pnpm -g; pnpm update -g; gh extension upgrade --all; omz update'
