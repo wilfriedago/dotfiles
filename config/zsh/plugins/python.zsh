fa68586d0e4220967e2330d3172ee73806f40c71
Wilfried AGO <68906789+wilfriedago@users.noreply.github.com>
2025-08-16 18:11:02 +0100

    refactor(python): reorganize plugin structure and checks
    - add check for python3 installation
    - improve organization of aliases and utils

--- config/zsh/plugins/python.zsh
+++ config/zsh/plugins/python.zsh
@@ -1,11 +1,26 @@
-#!/usr/bin/env zsh
+# =============================================================================================
+# ~/.config/zsh/plugins/python.zsh
+# =============================================================================================
+# Python Zsh Plugin
+# This plugin provides aliases and completion for Python.
+#
+# It makes managing Python environments and packages directly from the command line easier.
+# For docs and more info, see: https://github.com/wilfriedago/dotfiles
+# =============================================================================================
+# License: MIT Copyright (c) 2025 Wilfried Kirin AGO <https://wilfriedago.me>
+# =============================================================================================
 
-# aliases
+# Check if Python is installed
+if (( ! $+commands[python3] )); then
+  return
+fi
+
+# Aliases
 alias python='python3'
 alias pip='pip3'
 alias py='python3'
 alias py2='python2'
 alias pir='pip install -r requirements.txt'
 
-# PYTHON
+# Utils
 [ -d "$PWD/.venv" ] && source "$PWD/.venv/bin/activate" # Automatically load Python virtual environment if available
