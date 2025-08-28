b58acb3fa241c1596811b0fbc9e3cc9efcff497b
Wilfried AGO <68906789+wilfriedago@users.noreply.github.com>
2025-08-28 09:26:13 +0100

    feat: adds gitlab zsh plugin
    - creates a zsh plugin for gitlab
    - provides aliases and completions for the gitlab cli
    - improves command-line management of gitlab projects

---
+++ config/zsh/plugins/gitlab.zsh
@@ -0,0 +1,28 @@
+# =============================================================================================
+# ~/.config/zsh/plugins/gitlab.zsh
+# =============================================================================================
+# GitLab Zsh Plugin
+# This plugin provides aliases and completion for GitLab.
+#
+# It makes managing GitLab projects directly from the command line easier.
+# For docs and more info, see: https://github.com/wilfriedago/dotfiles
+# =============================================================================================
+# License: MIT Copyright (c) 2025 Wilfried Kirin AGO <https://wilfriedago.me>
+# =============================================================================================
+
+# Check if GitLab CLI is installed
+if (( !$+commands[glab] )); then
+  return
+fi
+
+# Aliases
+alias gl='glab'
+
+# Completions
+if [[ ! -f "$ZSH_CACHE_DIR/completions/_glab" ]]; then
+  typeset -g -A _comps
+  autoload -Uz _glab
+  _comps[glab]=_glab
+  _comps[gl]=_glab
+  glab completion -s zsh >| "$ZSH_CACHE_DIR/completions/_glab" &|
+fi
