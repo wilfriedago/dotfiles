026740a8c50fc639a0aeca92953272923d00a075
Wilfried AGO <68906789+wilfriedago@users.noreply.github.com>
2025-11-08 01:55:39 +0100

    feat(kind): add Kind Zsh plugin for aliases and completion

---
+++ config/zsh/plugins/kind.zsh
@@ -0,0 +1,26 @@
+# =============================================================================================
+# ~/.config/zsh/plugins/kind.zsh
+# =============================================================================================
+# Kind Zsh Plugin
+# This plugin provides aliases and completion for Kind.
+#
+# It makes managing Kubernetes clusters directly from the command line easier.
+# For docs and more info, see: https://github.com/wilfriedago/dotfiles
+# =============================================================================================
+# License: MIT Copyright (c) 2025 Wilfried Kirin AGO <https://wilfriedago.me>
+# =============================================================================================
+
+# Check if Kind is installed
+if (( ! $+commands[kind] )); then
+  return
+fi
+
+# =============================================================================================
+# Completions
+# =============================================================================================
+if [[ ! -f "$ZSH_CACHE_DIR/completions/_kind" ]]; then
+  typeset -g -A _comps
+  autoload -Uz _kind
+  _comps[kind]=_kind
+  kind completion zsh >| "$ZSH_CACHE_DIR/completions/_kind" &|
+fi
