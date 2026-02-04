206f52ff46c78d1ad3001e35306cdb9594b7a2f9
Wilfried AGO <68906789+wilfriedago@users.noreply.github.com>
2026-02-04 13:15:22 +0100

    fix(pnpm): correct alias for pnpm dlx command

--- config/zsh/plugins/pnpm.zsh
+++ config/zsh/plugins/pnpm.zsh
@@ -34,7 +34,7 @@ alias pt='pnpm test'
 alias pb='pnpm build'
 alias pu='pnpm update'
 alias po='pnpm outdated'
-alias pnx='pnpm dlx'
+alias px='pnpm dlx'
 
 # =============================================================================================
 # Completions
