dcd7b6c0d7f50102fbd7824a5442f32d6caed45e
Wilfried AGO <68906789+wilfriedago@users.noreply.github.com>
2026-05-23 09:34:10 +0100

    feat(zsh): remove unused eza aliases and mise plugin from zshrc

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -1,20 +1,8 @@
-# eza
-alias l="eza -al --icons" # ls -l with icons
-alias ll='eza -l --icons' # ls long format with icons
-alias la='eza -a --icons' # ls all files with icons
-alias ls='eza -a --icons --level=1' # ls with icons
-alias lt="eza -al --icons --level=3 --tree --git-ignore" # ls tree with icons
-
 # dotdrop
 alias dot='dotdrop --profile=default --cfg=~/.dotfiles/dotdrop.config.yml' # dotdrop with default profile
 alias doti='dot install' # install dotfiles
 alias dotu='dot update' # update dotfiles
 
-# dotenv
-alias envlint='dotenv-linter'
-alias envvault='dotenv-vault'
-
-alias mw='./mvnw' # run Maven Wrapper
 alias cat='bat' # cat with syntax highlighting
 alias lzd="lazydocker" # run Docker TUI
 alias rm='trash' # move to trash
@@ -28,21 +16,12 @@ alias speedtest="curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/m
 alias flushdns="sudo killall -HUP mDNSResponder" # flush dns cache
 alias pubkey="cat ~/.ssh/id_ed25519.pub | pbcopy | echo 'Public Key => Copied to clipboard.'" # copy ssh public key to clipboard
 alias ports='lsof -iTCP -sTCP:LISTEN -n -P'
-alias diskusage='du -sh * | sort -hr'
 
 # Get macOS Software Updates, and update installed Homebrew, npm, and their installed packages
 alias update='sudo softwareupdate -i -a;'
 
 alias brewupdate='brew update; brew upgrade; brew cleanup' # update Homebrew
 alias ghupdate='gh extension upgrade --all' # update GitHub CLI extensions
-alias npmupdate='npm install -g npm; npm update -g' # update npm and global packages
-alias pnpmupdate='pnpm add -g pnpm; pnpm update -g' # update pnpm and global packages
-
-# history
-alias hi='history'
-alias hil='history | less'
-alias his='history | grep'
-alias hisi='history | grep -i'
 
 # Print each PATH entry on a separate line
 alias path='echo -e ${PATH//:/\\n}'
